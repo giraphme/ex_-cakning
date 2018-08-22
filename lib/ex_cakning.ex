078918faa03d82f504c118d81c53d82124ec245f
TamakiMaeda <giraph.me@gmail.com>
2018-08-22 13:04:34 +0900

    First commit

---
+++ lib/ex_cakning.ex
@@ -0,0 +1,18 @@
+defmodule ExCakning do
+  @moduledoc """
+  Documentation for ExCakning.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCakning.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_cakning_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCakningTest do
+  use ExUnit.Case
+  doctest ExCakning
+
+  test "greets the world" do
+    assert ExCakning.hello() == :world
+  end
+end
