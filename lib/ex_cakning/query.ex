ce07d8b094fd5f8a3deb46d9d48cb4562a0c9ce4
TamakiMaeda <giraph.me@gmail.com>
2018-08-22 18:14:39 +0900

    Add query utility

---
+++ lib/ex_cakning/query.ex
@@ -0,0 +1,54 @@
+defmodule ExCakning.Query do
+  alias ExCakning.Config
+
+  def put_api_key(params) when is_map(params) do
+    params
+    |> Enum.into(%{})
+    |> Map.merge(%{api_key: Config.api_key()})
+  end
+
+  @spec encode(nil) :: nil
+  def encode(nil), do: nil
+
+  @spec encode(list(any)) :: String.t()
+  def encode(params) when is_list(params) do
+    params
+    |> Enum.into(%{})
+    |> encode()
+  end
+
+  @spec encode(map()) :: String.t()
+  def encode(params) when is_map(params) do
+    params
+    |> Enum.map(fn
+      {k, v} when is_list(v) or is_map(v) -> encode_nested([k], v)
+      {k, v} -> encode_kv(k, v)
+    end)
+    |> List.flatten()
+    |> Enum.join("&")
+  end
+
+  defp encode_nested(keys, value) do
+    value
+    |> Enum.map(fn
+      {k, v} when is_list(v) or is_map(v) -> encode_nested(keys ++ [k], v)
+      {k, v} -> encode_kv(keys ++ [k], v)
+      v when is_list(v) or is_map(v) -> encode_nested(keys ++ [nil], v)
+      v -> encode_kv(keys ++ [nil], v)
+    end)
+  end
+
+  defp encode_kv(key, value) when is_list(key), do: "#{encode_key(key)}=#{to_url_safe(value)}"
+
+  defp encode_kv(key, value), do: "#{to_url_safe(key)}=#{to_url_safe(value)}"
+
+  defp encode_key(keys) when is_list(keys) do
+    [head | tail] = Enum.map(keys, &to_url_safe/1)
+    children = tail |> Enum.map(&"[#{&1}]") |> Enum.join()
+    "#{head}#{children}"
+  end
+
+  defp to_url_safe(str) when is_binary(str), do: URI.encode_www_form(str)
+  defp to_url_safe(str) when is_binary(str), do: URI.encode_www_form(str)
+  defp to_url_safe(str), do: str |> to_string() |> URI.encode_www_form()
+end
---
+++ test/ex_cakning/query_test.exs
@@ -0,0 +1,22 @@
+defmodule ExCakning.QueryTest do
+  use ExUnit.Case
+  alias ExCakning.{Query, Config}
+
+  describe "ExCakning.Query.put_api_key/1" do
+    test "It sholuld change to string" do
+      assert Query.put_api_key(%{}) === %{api_key: Config.api_key()}
+    end
+  end
+
+  describe "ExCakning.Query.encode/1" do
+    test "It sholuld change to string" do
+      assert Query.encode(%{
+               name: "sample item=",
+               page: 1,
+               courier: [1, "POS INDONESIA"],
+               some_map: %{a: "value value", b: 1}
+             }) ==
+               "courier[]=1&courier[]=POS+INDONESIA&name=sample+item%3D&page=1&some_map[a]=value+value&some_map[b]=1"
+    end
+  end
+end
