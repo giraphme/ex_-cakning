1065d107a1f834c73dd9d45e2310f393fee8529e
TamakiMaeda <giraph.me@gmail.com>
2018-08-23 14:33:25 +0900

    Add Request.fetch/3 to make the API specification a blackbox

--- lib/ex_cakning/request.ex
+++ lib/ex_cakning/request.ex
@@ -1,6 +1,34 @@
 defmodule ExCakning.Request do
-  use HTTPotion.Base
-  alias ExCakning.Config
+  use HTTPoison.Base
+  alias ExCakning.{Config, Query, Response}
+
+  def fetch(method, url, params \\ [])
+
+  def fetch(:get, url, params) do
+    query_string =
+      params
+      |> Enum.into(%{})
+      |> Query.put_api_key()
+      |> Query.encode()
+
+    "#{url}?#{query_string}"
+    |> get()
+    |> Response.cast()
+  end
+
+  def fetch(method, url, params) do
+    # Unfortunately, we have to use query string even if we use POST method :(
+    # On top of that we have to put API token on query string. >-<
+    query_string =
+      params
+      |> Enum.into(%{})
+      |> Query.put_api_key()
+      |> Query.encode()
+
+    __MODULE__
+    |> apply(method, ["#{url}?#{query_string}", ""])
+    |> Response.cast()
+  end
 
   def process_url(url) do
     Config.base_url() <> url
@@ -11,7 +39,16 @@ defmodule ExCakning.Request do
     |> Keyword.put(:"Content-Type", "application/json")
   end
 
+  def process_request_body(body) when is_map(body) or is_list(body) do
+    Jason.encode!(body)
+  end
+
+  def process_request_body(body), do: body
+
   def process_response_body(body) do
-    Jason.decode!(body)
+    case Jason.decode(body) do
+      {:ok, json} -> json
+      {:error, _} -> body
+    end
   end
 end
