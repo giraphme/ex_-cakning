1c57377dadb6bc409819de798b63d383b9115650
TamakiMaeda <giraph.me@gmail.com>
2018-08-23 15:03:25 +0900

    Add interfaces for cakning endpoints

--- test/ex_cakning_test.exs
+++ test/ex_cakning_test.exs
@@ -1,3 +1,23 @@
 defmodule ExCakningTest do
   use ExUnit.Case
+  alias ExCakning.Response
+
+  describe "ExCakning.products_all" do
+    test "It should return {:ok, %ExCakning.Response{}}" do
+      assert {:ok, %Response{}} = ExCakning.products_all()
+    end
+  end
+
+  describe "ExCakning.products" do
+    test "It should return {:ok, %ExCakning.Response{}}" do
+      assert {:ok, %Response{}} = ExCakning.products()
+    end
+  end
+
+  @sample_product_id 29766
+  describe "ExCakning.product_detail" do
+    test "It should return {:ok, %ExCakning.Response{}}" do
+      assert {:ok, %Response{}} = ExCakning.product_detail(@sample_product_id)
+    end
+  end
 end
